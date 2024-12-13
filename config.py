c8e1019a770c08f9bcf4d0f746791e9e15dc8959
emiranivala <taitufam@gmail.com>
2024-12-13 16:14:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "24210243"))
+API_ID = int(os.environ.get("API_ID", "11693223"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "509031fb3790b968e489f71d591ebce5")
+API_HASH = os.environ.get("API_HASH", "495b1c6af3a3a250b06d07049f32ec80")
 
 #Database 
 DB_URI = os.environ.get("DB_URI", "")
